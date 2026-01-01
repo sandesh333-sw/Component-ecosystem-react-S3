5b32d918980d2ebf9750457bca8161b6d5115a9c
Sandesh Pulami <sandeshpulami333@gmail.com>
2026-01-01 18:19:40 +0000

    feat: integrate props

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,16 @@ function App() {
     <>
       <h1 className="text-blue-600 dark:text-sky-400">Learn to integrate tailwind</h1>
       <div className='flex gap-4'>
-        <Card />
-        <Card />
+        <Card 
+          title='Buy Python course'
+          buttonText='join now' 
+          imageUrl="https://media.istockphoto.com/id/1340516764/photo/empty-classroom-ready-for-pupils.jpg?s=2048x2048&w=is&k=20&c=Utnq9LnSoVro9b9rl1blTcjI1apDd3oDZOK6JUpplPA="
+          />
+        <Card
+          title='Buy Node.js course'
+          buttonText='join today' 
+          imageUrl="https://media.istockphoto.com/id/2203424023/photo/businesswoman-leading-seminar-activities-with-flipchart-at-convention.jpg?s=2048x2048&w=is&k=20&c=uOrVBdM2q5Z39gdkiBKyJVFwQwC25jQueguHPeH7aHE="
+        />
       </div>
     </>
   )
